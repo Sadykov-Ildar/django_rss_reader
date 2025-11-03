1d8ed7811b43ed03a9bab1d6f6500e42b81e7939
Sadykov Ildar <sadykov.ildar.dev@gmail.com>
2025-11-03 19:13:10 +0300

    replaces some js hacks with proper configs for htmx

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,12 +1,3 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.body.addEventListener('htmx:beforeSwap', function (evt) {
-        if (evt.detail.xhr.status === 422) {
-            evt.detail.shouldSwap = true;
-            evt.detail.isError = false;
-        }
-    });
-})
-
 function deselect_active_elements(css_class) {
     let elements = document.querySelectorAll("." + css_class);
     elements.forEach(element => {
