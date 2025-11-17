6a088dcf3a5d8ae2fc5095f358a959e171fe90a8
Sadykov Ildar <sadykov.ildar.dev@gmail.com>
2025-11-17 16:43:35 +0300

    removes unneeded TODOs (tested granian, seems unnecessary at the moment)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,4 @@ python manage.py migrate --no-input
 
 python manage.py collectstatic --no-input
 
-# TODO: fastwsgi, granian?
-# TODO: потестить с https://github.com/rakyll/hey
 gunicorn django_rss_reader.wsgi --bind 0.0.0.0:8000
