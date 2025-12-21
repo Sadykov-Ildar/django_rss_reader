54d05e0c9f7f36eec91e4577d5ec4f357e85c209
Sadykov Ildar <sadykov.ildar.dev@gmail.com>
2025-12-21 17:21:23 +0300

    replaces WebsocketConsumer with AsyncWebsocketConsumer

--- src/rss_reader/websockets/consumers.py
+++ src/rss_reader/websockets/consumers.py
@@ -1,12 +1,11 @@
 import json
 
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 from rss_reader.constants import WS_TASKS_REFRESHED_GROUP_NAME
 
 
-class BackgroundTaskFinishNotifier(WebsocketConsumer):
+class BackgroundTaskFinishNotifier(AsyncWebsocketConsumer):
     """
     Sends notification about finishing updates to all users at the same time,
     who then will reload page and DDoS our site :)
@@ -14,20 +13,18 @@ class BackgroundTaskFinishNotifier(WebsocketConsumer):
     Could be implemented better, but I am the only user, so it doesn't matter.
     """
 
-    def connect(self):
+    async def connect(self):
         self.group_name = WS_TASKS_REFRESHED_GROUP_NAME
 
-        async_to_sync(self.channel_layer.group_add)(self.group_name, self.channel_name)
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, code):
-        async_to_sync(self.channel_layer.group_discard)(
-            self.group_name, self.channel_name
-        )
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
-    def tasks_refreshed(self, event):
+    async def tasks_refreshed(self, event):
         message = event["message"]
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps({"message": message}))
