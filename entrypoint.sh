a203f5c2f62cd567662a01f557790f5fdcec8f99
Sadykov Ildar <sadykov.ildar.dev@gmail.com>
2026-01-12 17:37:50 +0300

    replaces granian with daphne
    it seems that granian had some issue that resulted in blocking event loop, completely stopping server from accepting any requests, and even connecting remote debugger

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py migrate --no-input
 
 python manage.py collectstatic --no-input
 
-granian --interface asgi --host 0.0.0.0 --port 8000 --workers 1 django_rss_reader.asgi:application
+daphne -b 0.0.0.0 -p 8000 django_rss_reader.asgi:application
