c50c844bf8be9ec913c566c40ebf217fd61d103f
Sadykov Ildar <sadykov.ildar.dev@gmail.com>
2026-01-12 17:38:48 +0300

    fixes potential KeyError in BackgroundTaskFinishNotifier

--- src/rss_reader/websockets/consumers.py
+++ src/rss_reader/websockets/consumers.py
@@ -24,7 +24,7 @@ class BackgroundTaskFinishNotifier(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def tasks_refreshed(self, event):
-        message = event["message"]
+        message = event.get("message", "")
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({"message": message}))
